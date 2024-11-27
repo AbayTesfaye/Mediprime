b8de44aa1c71e4af40a13b7523103d9a2fc92bfa
AbayTesfaye <abaytesfaye02@gmail.com>
2024-11-27 18:44:32 +0300

    Animate the why-us page using Farmer motion

--- src/components/why-us.tsx
+++ src/components/why-us.tsx
@@ -3,8 +3,35 @@ import emergency from "../assets/doc-0.jpg"
 import dentistry from "../assets/doc-1.jpg"
 import surgical from "../assets/doc-2.jpg"
 import health from "../assets/doc-3.jpg"
+
+import {motion, useMotionValueEvent, useScroll, useTransform} from "framer-motion";
+import { useRef,useState } from "react"
 const WhyUs = () => {
-  
+  const cardRef = useRef<HTMLDivElement | null>(null);
+  const [position, setPosition] = useState(1);
+
+  const {scrollYProgress} = useScroll({
+    target: cardRef,
+    offset: ["start center", "end start"]
+  });
+
+  const height = useTransform(
+    scrollYProgress,
+     [0, 0.5],
+    ["12.91512915129151%","100%"]
+  );
+
+  useMotionValueEvent(scrollYProgress,"change",(latest)=>{
+    if(latest< 0.125){
+      setPosition(1);
+    }else if(latest> 0.125 && latest < 0.25){
+      setPosition(2);
+    }else if(latest> 0.25 && latest < 0.375){
+      setPosition(3);
+    }else{
+      setPosition(4);
+    }
+  })
   return (
     <section
     id="services"
@@ -14,12 +41,32 @@ const WhyUs = () => {
       <div className="relative flex w-full flex-col-reverse gap-16 md:flex-row 3xl:max-w-[1200px]">
 
         {/* Left pannel content */}
-        <div className="relative z-10 h-[350px] w-full object-cover sm:h-[450px] md:h-[591px] md:min-w-[400px] xl:min-w-[592px]">
-         <img src={emergency} alt="" className="absolute z-[13] h-full scale-[1.01] object-cover transition-all duration-300"/>
-        </div>
+        <motion.div
+           initial ={{opacity:0}}
+           whileInView ={{
+             opacity:1,
+             transition:{
+               duration:0.5,
+               delay:0.3,
+               ease: [0.44,0,0.22,0.99], 
+             }
+           }}
+           viewport={{
+             amount:"50%",
+             once: true
+           }}
+         className="relative z-10 h-[350px] w-full object-cover sm:h-[450px] md:h-[591px] md:min-w-[400px] xl:min-w-[592px]">
+         <img src={emergency} alt=""  className={`absolute z-[14] h-full scale-[1.01] object-cover transition-all duration-300 ${position === 1 ? "opacity-100" : "opacity-0"}`} />
+
+         <img src={dentistry} alt="" className={`absolute z-[13] h-full scale-[1.01] object-cover transition-all duration-300 ${position === 1 ? "bg-opacity-100" : "opacity-0"}`}/>
+
+         <img src={surgical} alt="" className={`absolute z-[11] h-full w-full object-cover transition-all duration-300 ${position <= 3 ? "bg-opacity-100" : "opacity-0"}`}/>
+
+         <img src={health} alt="" className={`absolute z-[10] h-full w-full object-cover transition-all duration-300 ${position <= 4 ? "bg-opacity-100" : "opacity-100"}`}/>
+        </motion.div>
           {/* Left pannel content */}
 
-        <div className="flex flex-col pt-[0px]">
+        <div ref={cardRef} className="flex flex-col pt-[0px]">
             <h3 className="text-[32px] font-bold leading-[44px] tracking-[-0.72px] text-[#0e1829] md:text-[36px]">
               Medical departments.
             </h3>
@@ -28,14 +75,33 @@ const WhyUs = () => {
             <div className="relative mt-6 flex w-full flex-col items-center justify-center gap-6">
                 {/* Border line content */}
                 <div className="[544px] absolute left-0 top-0 h-full w-1 bg-[#eaecf0]">
-                   <div className="h-[70px] w-1 rounded-full bg-violet-700"/>
+                   <motion.div className="h-[70px] w-1 rounded-full bg-violet-700"
+                   style={{height:height}}/>
                 </div>
                 {/* Border line content */}
 
-                <div className="py-4 pl-6 transition-all">
+                <div className={`py-4 pl-6 transition-all ${position === 1 ? "opacity-100" : "opacity-30"}`}>
                   <h5 className="text-[20px] font-bold leading-[30px] text-[#0e1829]">Emergency Department</h5>
                   <p className="mt-2 text-base font-medium leading-6 text-[#445469]">
                     Even the all-powerfull pointing has no control about the blind texts it is an almost unorthographic life.
+                  </p>
+                </div>
+                <div className={`py-4 pl-6 transition-all ${position === 2 ? "opacity-100" : "opacity-30"}`}>
+                  <h5 className="text-[20px] font-bold leading-[30px] text-[#0e1829]">Restorative Density</h5>
+                  <p className="mt-2 text-base font-medium leading-6 text-[#445469]">
+                    Our dedicated team of medical professionals is here to guide you on your health journey.
+                  </p>
+                </div>
+                <div className={`py-4 pl-6 transition-all ${position === 3 ? "opacity-100" : "opacity-30"}`}>
+                  <h5 className="text-[20px] font-bold leading-[30px] text-[#0e1829]">Surgical Interventions</h5>
+                  <p className="mt-2 text-base font-medium leading-6 text-[#445469]">
+                    Even the all-powerfull pointing has no control about the blind texts it is an almost unorthographic life.
+                  </p>
+                </div>
+                <div className={`py-4 pl-6 transition-all ${position === 4 ? "opacity-100" : "opacity-30"}`}>
+                  <h5 className="text-[20px] font-bold leading-[30px] text-[#0e1829]">Health Monitoring</h5>
+                  <p className="mt-2 text-base font-medium leading-6 text-[#445469]">
+                  Our dedicated team of medical professionals is here to guide you on your health journey.
                   </p>
                 </div>
             </div>
